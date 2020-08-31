9fdfcb413dab210a8951ad3e41c06e8d161da352
Vanessa <va.au@gmx.at>
2020-08-31 08:41:02 +0200

    revised

--- src/Numbers.java
+++ src/Numbers.java
@@ -5,18 +5,21 @@ public class Numbers {
         int luckyNumber = 15;
 
         Scanner scannerVariable = new Scanner(System.in);
-        int  number = scannerVariable.nextInt();
+        int number = scannerVariable.nextInt();
 
-        if (number%10 == 0){
-            System.out.printf("%d is a round number.", number);
+        if (number % 10 == 0) {
+            System.out.printf("%d is a round number.%n", number);
 
-        } else if (number%2 == 0) {
-            System.out.printf("%d is an even number.", number);
-        } else if (number == luckyNumber) {
-            System.out.printf("%d is my lucky number.", number);
+        }
+        if (number % 2 == 0) {
+            System.out.printf("%d is an even number.%n", number);
+        }
+        if (number == luckyNumber) {
+            System.out.printf("%d is my lucky number.%n", number);
 
-        } else if (number>9 && number<100) {
-            System.out.printf("%d has 2 digits.", number);
+        }
+        if (number > 9 && number < 100) {
+            System.out.printf("%d has 2 digits.%n", number);
         }
     }
 }
